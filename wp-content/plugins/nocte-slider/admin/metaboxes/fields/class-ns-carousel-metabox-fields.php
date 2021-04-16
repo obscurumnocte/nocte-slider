05c1961deb58cff7914a1ae0647d212c9a0bdb07
nb_TSU <nigel@thesetupdesign.co.uk>
2021-04-17 00:25:27 +0100

    Clean up some comments

--- wp-content/plugins/nocte-slider/admin/metaboxes/fields/class-ns-carousel-metabox-fields.php
+++ wp-content/plugins/nocte-slider/admin/metaboxes/fields/class-ns-carousel-metabox-fields.php
@@ -46,8 +46,6 @@ class NS_Carousel_Metabox_Fields {
      * Check field options are valid and check field method exists
      */
     public function check_options_method( $field_name, $field_options ){
-    	global $post;
-
         //  Check field_name, empty, lowercase, no spaces
         if( empty( $field_name ) || is_numeric( $field_name ) ){
             //  Display error for invalid field name
@@ -62,7 +60,6 @@ class NS_Carousel_Metabox_Fields {
         }
         //  Create field specific class name, check it exists and to field list
         $class_name = 'NS_Carousel_Field_'. ucfirst( $field_options['type'] );
-        //print'<pre>class name = '.$class_name.'</pre>';
         if( !class_exists( $class_name ) ){
             //  Display error for unrecognised field type
             $this->display_error( __('Field type not recognised:', 'ns') .' '. $field_name );
@@ -70,8 +67,6 @@ class NS_Carousel_Metabox_Fields {
         }
         //  Call field type method
         $this->fields[] = new $class_name( $field_name, $field_options, $post->ID );
-        //print'<pre>fields = '.print_r($this->fields,true).'</pre>';
-        //print'<pre>field = '.print_r($this->fields[ count( $this->fields ) - 1 ],true).'</pre>';
         $this->fields[ count( $this->fields ) - 1 ]->display_field();
 
         return true;
