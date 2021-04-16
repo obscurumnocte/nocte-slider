9d027869f8405e98fec88fbda2f8187ba6751736
nb_TSU <nigel@thesetupdesign.co.uk>
2021-04-17 00:32:21 +0100

    Count, on load, repeater blocks to add number to new repeater blocks fields

--- wp-content/plugins/nocte-slider/admin/js/nocte-slider-admin.js
+++ wp-content/plugins/nocte-slider/admin/js/nocte-slider-admin.js
@@ -4,6 +4,14 @@
 	 //$( window ).load(function(){});
 	$(document).ready(function(){
 
+		//  Find how many repeater fields sets there are to track naming counter
+		var $repeater_values = $('.repeater-values-wrapper');
+		if( $repeater_values.length > 0 ){
+			$repeater_values.each(function(){
+				$(this).attr('data-count', $(this).children().length );
+			});
+		}
+
 		// Field Repeater functions
 		//  Generic add item button
 		var $add_item = $('.add-repeater-item');
@@ -11,7 +19,22 @@
 			$add_item.on('click', function(e){
 				e.preventDefault();
 				var $item = $(this).parent().siblings('.repeater-template');
-				$item.siblings('.repeater-values-wrapper').append( $item.clone().removeClass('repeater-template') );
+				var $new_item = $item.clone().removeClass('repeater-template');
+				var $repeater_values_wrap = $item.siblings('.repeater-values-wrapper');
+				var current_count = $repeater_values_wrap.attr('data-count');
+				// Need to update field names for repeater
+				$new_item.find('input,textarea,select').each(function(){
+					var $this = $(this);
+					var input_name = $this.attr('name');
+					var pos_bracket = input_name.indexOf('[');
+					if( pos_bracket != -1 ){
+						var input_name_start = input_name.substring( 0, pos_bracket +1 );
+						var input_name_end = input_name.substring( pos_bracket +1 );
+						$this.attr('name', input_name_start + current_count + input_name_end );
+					}
+				});
+				$repeater_values_wrap.append( $new_item );
+				$repeater_values_wrap.attr('data-count', current_count +1 );
 			});
 		}
 
