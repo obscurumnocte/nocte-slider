640ee59d7f30c878889d550d788cb0898e1b5ee1
nb_TSU <nigel@thesetupdesign.co.uk>
2021-07-05 00:23:14 +0100

    Queue Owl Carousel styles and JS

--- wp-content/plugins/nocte-slider/public/class-nocte-slider-public.php
+++ wp-content/plugins/nocte-slider/public/class-nocte-slider-public.php
@@ -25,6 +25,10 @@ class Nocte_Slider_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles(){
+		//  Add Owl Carousel files
+		wp_enqueue_style( Nocte_Slider_Data::get_plugin_name() .'-owl', plugin_dir_url( __FILE__ ) .'vendors/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css', array(), '2.3.4', 'all');
+		wp_enqueue_style( Nocte_Slider_Data::get_plugin_name() .'-owl-theme', plugin_dir_url( __FILE__ ) .'vendors/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css', array(), '2.3.4', 'all');
+		//  Add plugin styles
 		wp_enqueue_style( Nocte_Slider_Data::get_plugin_name(), plugin_dir_url( __FILE__ ) .'css/nocte-slider-public.css', array(), Nocte_Slider_Data::get_plugin_version(), 'all');
 	}
 
@@ -34,6 +38,9 @@ class Nocte_Slider_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts(){
+		//  Add Owl Carousel files
+		wp_enqueue_script( Nocte_Slider_Data::get_plugin_name() .'-owl-js', plugin_dir_url( __FILE__ ) .'vendors/OwlCarousel2-2.3.4/dist/owl.carousel.min.js', array('jquery'), '2.3.4', false );
+		//  Add plugin styles
 		wp_enqueue_script( Nocte_Slider_Data::get_plugin_name(), plugin_dir_url( __FILE__ ) .'js/nocte-slider-public.js', array('jquery'), Nocte_Slider_Data::get_plugin_version(), false );
 	}
 
